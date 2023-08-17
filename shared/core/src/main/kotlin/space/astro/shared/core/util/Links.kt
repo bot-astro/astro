739fc04fc6fc6ccbd7f345307afdbfa24fe007ce
Giuliopime <60524738+Giuliopime@users.noreply.github.com>
2023-08-17 13:16:28 +0200

    feat: ui components

---
+++ shared/core/src/main/kotlin/space/astro/shared/core/util/Links.kt
@@ -0,0 +1,29 @@
+package space.astro.shared.core.util
+
+/**
+ * Utility class for Astro most used links
+ */
+object Links {
+    const val base = "https://astro-bot.space"
+    const val dashboard = "$base/dashboard"
+    const val invite = "https://discord.com/oauth2/authorize?client_id=715621848489918495&scope=bot+applications.commands&permissions=8"
+    const val support = "https://discord.gg/yeXwVhg"
+    const val premium = "$base/premium"
+    const val appDirectoryPremium = "https://discord.com/application-directory/715621848489918495/premium"
+    const val docs = "https://docs.astro-bot.space"
+    const val deprecatedPremium = "$docs/premium/#old-premium-members"
+
+    const val logo = "https://cdn.discordapp.com/avatars/715621848489918495/dc0affdf8de07a3d88c4d192efad649f.png?size=2048"
+    const val interfaceInstructions = "$base/interface-image.png"
+
+    const val topgg = "https://top.gg/bot/715621848489918495"
+    const val topggLogo = "https://cdn.top.gg/icons/7b707bf868adef7730776f171a2dc709.jpeg"
+    const val vote = "$topgg/vote"
+    const val github = "https://github.com/bot-astro"
+
+    const val developer = "https://giuliopime.dev"
+    const val developerLogo = "https://cdn.discordapp.com/avatars/256851887502917633/7750731e71f8634403ef6d078a71614e.png"
+
+    const val roleHierarchy = "https://support.discord.com/hc/en-us/articles/214836687-Role-Management-101"
+    const val getId = "https://support.discord.com/hc/en-us/articles/206346498-Where-can-I-find-my-User-Server-Message-ID-"
+}
