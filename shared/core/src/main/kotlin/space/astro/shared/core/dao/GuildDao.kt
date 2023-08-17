06f474386fd908d21bcdddb88d937efd47fca6e6
Giuliopime <60524738+Giuliopime@users.noreply.github.com>
2023-08-17 15:59:37 +0200

    feat: kotlin mongo driver

---
+++ shared/core/src/main/kotlin/space/astro/shared/core/dao/GuildDao.kt
@@ -0,0 +1,23 @@
+package space.astro.shared.core.dao
+
+import com.mongodb.client.MongoCollection
+import com.mongodb.client.MongoDatabase
+import com.mongodb.client.model.FindOneAndReplaceOptions
+import com.mongodb.client.model.FindOneAndUpdateOptions
+import com.mongodb.client.model.Filters.*
+import com.mongodb.client.model.Indexes
+import com.mongodb.client.model.ReturnDocument
+import org.springframework.stereotype.Component
+import space.astro.shared.core.models.database.GuildDto
+
+@Component
+class GuildDao(
+    mongoDatabase: MongoDatabase
+) {
+    lateinit var collection: MongoCollection<GuildDto>
+
+    init {
+        collection = mongoDatabase.getCollection("guilds", GuildDto::class.java)
+        collection.createIndex(Indexes.ascending(GuildDto::guildID.name))
+    }
+}
