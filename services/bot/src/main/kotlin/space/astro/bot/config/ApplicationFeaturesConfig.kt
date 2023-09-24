08fa2c074e74e6101957eba15f5eaa53e5ca6c9f
Giuliopime <60524738+Giuliopime@users.noreply.github.com>
2023-09-24 10:51:46 +0200

    feat: application feature configs

---
+++ services/bot/src/main/kotlin/space/astro/bot/config/ApplicationFeaturesConfig.kt
@@ -0,0 +1,10 @@
+package space.astro.bot.config
+
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+@ConfigurationProperties("application.features")
+class ApplicationFeaturesConfig {
+    val premiumRestrictions: Boolean = true
+}
