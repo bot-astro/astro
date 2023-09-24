5c7bcff6028d6e0dad6a605e80c1b301046dbeb1
Giuliopime <60524738+Giuliopime@users.noreply.github.com>
2023-09-24 10:52:02 +0200

    fix: mongo lib

--- shared/core/src/main/kotlin/space/astro/shared/core/components/mongo/MongoFactory.kt
+++ shared/core/src/main/kotlin/space/astro/shared/core/components/mongo/MongoFactory.kt
@@ -2,9 +2,8 @@ package space.astro.shared.core.components.mongo
 
 import com.mongodb.ConnectionString
 import com.mongodb.MongoClientSettings
-import com.mongodb.client.MongoClient
-import com.mongodb.client.MongoClients
-import com.mongodb.client.MongoDatabase
+import com.mongodb.kotlin.client.MongoClient
+import com.mongodb.kotlin.client.MongoDatabase
 import org.bson.codecs.configuration.CodecRegistries
 import org.bson.codecs.pojo.PojoCodecProvider
 import org.springframework.context.annotation.Bean
@@ -15,7 +14,7 @@ class MongoFactory {
 
     @Bean
     fun getMongoClient(mongoConfig: MongoConfig): MongoClient {
-        return MongoClients.create(
+        return MongoClient.create(
             MongoClientSettings.builder()
                 .applyConnectionString(
                     ConnectionString(mongoConfig.connectionString)
