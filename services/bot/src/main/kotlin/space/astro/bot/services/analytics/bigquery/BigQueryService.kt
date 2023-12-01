22b0866a167167541aedc6db151e0da74fbcdddd
Xavin <xavin@hydra.bot>
2023-12-01 11:12:55 +0100

    feat: implement bigquery client

---
+++ services/bot/src/main/kotlin/space/astro/bot/services/analytics/bigquery/BigQueryService.kt
@@ -0,0 +1,35 @@
+package space.astro.bot.services.analytics.bigquery
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.google.cloud.bigquery.BigQuery
+import space.astro.shared.core.models.structures.BigQueryBaseClient
+import org.springframework.stereotype.Service
+import space.astro.shared.core.configs.BigQueryConfig
+import space.astro.shared.core.models.analytics.AnalyticsEventType
+import space.astro.shared.core.models.analytics.SlashCommandInvocationEventData
+import java.util.*
+import kotlin.reflect.KClass
+
+@Service
+class BigQueryService(
+    bigQuery: BigQuery?,
+    bigQueryConfig: BigQueryConfig,
+    objectMapper: ObjectMapper
+) :
+    BigQueryBaseClient(
+        TABLES,
+        bigQuery, bigQueryConfig, objectMapper
+    ) {
+    companion object {
+        private val TABLES: Map<KClass<*>, AnalyticsEventType>
+
+        init {
+            val tables: Map<KClass<*>, AnalyticsEventType> = HashMap(
+                mapOf(
+                    SlashCommandInvocationEventData::class to AnalyticsEventType.SLASH_COMMAND_INVOCATION
+                )
+            )
+            TABLES = Collections.unmodifiableMap(tables)
+        }
+    }
+}
---
+++ shared/core/src/main/kotlin/space/astro/shared/core/models/analytics/AnalyticsEventType.kt
@@ -0,0 +1,6 @@
+package space.astro.shared.core.models.analytics
+
+enum class AnalyticsEventType {
+
+    SLASH_COMMAND_INVOCATION
+}
