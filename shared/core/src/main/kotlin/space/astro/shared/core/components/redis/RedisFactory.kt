03f4619ad04ab80869e44a6c901b720d82714e2e
Xavin <xavin@hydra.bot>
2024-02-09 12:29:42 +0100

    chore: add logs for redis connection

--- shared/core/src/main/kotlin/space/astro/shared/core/components/redis/RedisFactory.kt
+++ shared/core/src/main/kotlin/space/astro/shared/core/components/redis/RedisFactory.kt
@@ -11,9 +11,12 @@ import io.lettuce.core.cluster.api.StatefulRedisClusterConnection
 import io.lettuce.core.cluster.api.async.RedisClusterAsyncCommands
 import io.lettuce.core.cluster.api.reactive.RedisClusterReactiveCommands
 import io.lettuce.core.cluster.api.sync.RedisClusterCommands
+import mu.KotlinLogging
 import org.springframework.context.annotation.Bean
 import org.springframework.stereotype.Component
 import java.time.Duration
+
+private val logger = KotlinLogging.logger { }
 
 /**
  * Service for communication with Redis
@@ -27,9 +30,15 @@ class RedisFactory(redisConfig: RedisConfig) {
     private var statefulRedisConnection: StatefulRedisConnection<String, String>? = null
 
     init {
+        logger.info { "Initializing Redis connection" }
+        logger.info { "Redis cluster: ${redisConfig.cluster}" }
+        logger.info { "Redis URIs: ${redisConfig.uris}" }
+
         val redisUris = redisConfig.uris.split(",").stream()
             .map(RedisURI::create)
             .toList()
+
+        logger.info { "Redis URIs: $redisUris" }
 
         if (redisConfig.cluster) {
             val clusterClient = RedisClusterClient.create(redisUris)
@@ -50,6 +59,13 @@ class RedisFactory(redisConfig: RedisConfig) {
             )
             statefulRedisClusterConnection = clusterClient.connect()
         } else {
+            logger.info { "Creating Redis client" }
+            logger.info { "Redis URI: ${redisUris[0]}" }
+            logger.info { "Redis Port: ${redisUris[0].port}" }
+            logger.info { "Redis Host: ${redisUris[0].host}" }
+            logger.info { "Redis Password: ${redisUris[0].password.joinToString()}" }
+            logger.info { "Redis Database: ${redisUris[0].database}" }
+            logger.info { "Redis Username: ${redisUris[0].username}" }
             val client = RedisClient.create(redisUris[0])
             statefulRedisConnection = client.connect()
         }
