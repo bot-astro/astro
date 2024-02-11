98a0d5f7979e1920300435a2983371c0b180c881
Giuliopime <60524738+Giuliopime@users.noreply.github.com>
2024-02-11 14:47:35 +0100

    fix: premium limit bug

--- services/bot/src/main/kotlin/space/astro/bot/components/managers/PremiumRequirementDetector.kt
+++ services/bot/src/main/kotlin/space/astro/bot/components/managers/PremiumRequirementDetector.kt
@@ -44,7 +44,7 @@ class PremiumRequirementDetector(
             return true
         }
 
-        return guildData.connections.size < 3
+        return guildData.generators.size < 3
     }
 
     fun canCreateInterface(guildData: GuildData): Boolean {
@@ -52,7 +52,7 @@ class PremiumRequirementDetector(
             return true
         }
 
-        return guildData.connections.size < 1
+        return guildData.interfaces.size < 1
     }
 
     fun canCreateTemplate(guildData: GuildData): Boolean {
