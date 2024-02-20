9f94954244175eea036c21cb56e702fa9340bd8e
Giuliopime <60524738+Giuliopime@users.noreply.github.com>
2024-02-20 01:44:26 +0100

    fix: json properties

--- shared/core/src/main/kotlin/space/astro/shared/core/models/discord/DiscordEntitlementData.kt
+++ shared/core/src/main/kotlin/space/astro/shared/core/models/discord/DiscordEntitlementData.kt
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.annotation.JsonProperty
 import java.time.OffsetDateTime
 
-@JsonIgnoreProperties
+@JsonIgnoreProperties(ignoreUnknown = true)
 data class DiscordEntitlementData(
     val id: String,
     @JsonProperty("sku_id")
