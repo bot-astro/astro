11ade2335521b8181d947ad3a7dc228a498dc5d3
Giuliopime <60524738+Giuliopime@users.noreply.github.com>
2024-05-09 17:59:11 +0200

    feat: guild id in debug logs

--- services/bot/src/main/kotlin/space/astro/bot/events/listeners/voice/handlers/LeftTemporaryVCEventHandler.kt
+++ services/bot/src/main/kotlin/space/astro/bot/events/listeners/voice/handlers/LeftTemporaryVCEventHandler.kt
@@ -29,7 +29,7 @@ fun VCEventHandler.handleLeftTemporaryVCEvent(
     if (newOwner == null) {
         temporaryVCDao.delete(guild.id, event.temporaryVCData.id)
 
-        log.info { "DELETE: Deleting temporary vc, detected ${vc.members.size} users, filtered to ${vc.members.filter { !it.user.isBot }.size} non bots" }
+        log.info { "DELETE: Deleting temporary vc, detected ${vc.members.size} users, filtered to ${vc.members.filter { !it.user.isBot }.size} non bots - server ${guild.id}" }
         vc.delete()
             .reason("Deleting temporary vc, detected ${vc.members.size} users, filtered to ${vc.members.filter { !it.user.isBot }.size} non bots")
             .queue()
